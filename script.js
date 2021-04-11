6b396f9cecddd9cc235add6e86011506419d0ff9
marcosbessa28 <81820354+marcosbessa28@users.noreply.github.com>
2021-04-11 18:11:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class AudioController {
         this.flipSound = new Audio('Assets/Audio/flip.wav');
         this.matchSound = new Audio('Assets/Audio/match.wav');
         this.victorySound = new Audio('Assets/Audio/victory.wav');
-        this.gameOverSound = new Audio('Assets/Audio/gameover.wav');
+        this.gameOverSound = new Audio('Assets/Audio/gameOver.wav');
         this.bgMusic.volume = 0.5;
         this.bgMusic.loop = true;
     }
